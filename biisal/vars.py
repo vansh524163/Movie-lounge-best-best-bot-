879f55e7a023d755781fffd6987ba271e72fe8f7
abdulkhalik88822 <125821386+abdulkhalik88822@users.noreply.github.com>
2024-09-04 09:52:51 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -18,8 +18,8 @@ class Var(object):
     name = str(getenv('name', 'File_Press_bot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002248984991'))
-    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1002248984991'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001626107740'))
+    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1001626107740'))
     PORT = int(getenv('PORT', '9097'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
