570fe9e250dc8a98d558ba9feb2eae835325868e
Illegal Developer / <147461995+IllegalDevelopers@users.noreply.github.com>
2024-09-10 19:30:29 +0530

    Update stream.py

--- biisal/bot/plugins/stream.py
+++ biisal/bot/plugins/stream.py
@@ -93,7 +93,7 @@ async def private_receive_handler(c: Client, m: Message):
         stream_link = f"https://ddbots.blogspot.com/p/stream.html?link={str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
         online_link = f"https://ddbots.blogspot.com/p/download.html?link={str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
         file_link = f"https://telegram.me/{Var.SECOND_BOTUSERNAME}?start=file_{log_msg.id}"
-        share_link = f"https://ddlink57.blogspot.com/{str(log_msg.id)}"
+        share_link = f"https://ddlink57.blogspot.com/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
 
         # Log the request in the BIN_CHANNEL
         await log_msg.reply_text(
@@ -114,7 +114,10 @@ async def private_receive_handler(c: Client, m: Message):
                         InlineKeyboardButton('Download 🔻', url=online_link)  # Download Link
                     ],
                     [
-                        InlineKeyboardButton('⚡ Share Link ⚡', url=file_link)  # Corrected Share Link Button
+                        InlineKeyboardButton('⚡ Share Link ⚡', url=share_link)
+                    ],
+                    [
+                        InlineKeyboardButton('Get File', url=file_link)
                     ]
                 ]
             )
