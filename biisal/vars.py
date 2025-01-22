acf4c024ef00721ef6d8c5a52f6f627ec57b1c90
vansh524163 <157708281+vansh524163@users.noreply.github.com>
2025-01-22 09:48:00 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -21,7 +21,7 @@ class Var(object):
     BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001995168865'))
     NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1001995168865'))
     PORT = int(getenv('PORT', '9097'))
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'ripe-daisie-vansh9842-cd76f4fd.koyeb.app'))
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "2020224264").split()]
     NO_PORT = bool(getenv('NO_PORT', False))
@@ -46,5 +46,5 @@ class Var(object):
     BAN_ALERT = str(getenv('BAN_ALERT' , '<b>ʏᴏᴜʀ ᴀʀᴇ ʙᴀɴɴᴇᴅ ᴛᴏ ᴜsᴇ ᴛʜɪs ʙᴏᴛ.Pʟᴇᴀsᴇ ᴄᴏɴᴛᴀᴄᴛ @none_090 ᴛᴏ ʀᴇsᴏʟᴠᴇ ᴛʜᴇ ɪssᴜᴇ!!</b>'))
     DB_URI = str(getenv('DB_URI', 'mongodb+srv://yewilis933:SGROmrioybQJAnWj@cluster0.96en1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'))
     DB_NAME = str(getenv("DB_NAME", "Vansh"))
-    SECOND_BOTUSERNAME = str(getenv("SECOND_BOTUSERNAME", "cinema4u_vansh_bot"))
-    FILE_BOT_TOKEN = str(getenv('FILE_BOT_TOKEN', '6992436830:AAHxSurOyULZv-Lv2nGtB38kXeHPLsuaeCg'))
+    SECOND_BOTUSERNAME = str(getenv("SECOND_BOTUSERNAME", "movie_hub09_bot"))
+    FILE_BOT_TOKEN = str(getenv('FILE_BOT_TOKEN', '6932919860:AAECwfGbxyS6b1fSFFinD_h0czZwp_Te84E'))
